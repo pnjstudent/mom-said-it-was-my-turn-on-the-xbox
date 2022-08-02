8a4fd06fa6a2592fee9b8ef0a0447e075226bbc7
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-08-02 15:47:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,12 @@
-basic.forever(function () {
-    led.setBrightness(input.soundLevel())
-    sound.mapImagesToVolume([
-    images.createImage(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `),
-    images.createImage(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `),
-    images.createImage(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `),
-    images.createImage(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `),
-    images.createImage(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    ])
+let timer = 0
+input.onButtonPressed(Button.A, function () {
+    timer = randint(5, 15)
+    basic.showIcon(IconNames.Yes)
+    while (0 < timer) {
+        timer += -1
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Forever)
+        basic.pause(1000)
+    }
+    music.stopAllSounds()
+    basic.showString("GAMEOVER!")
 })
